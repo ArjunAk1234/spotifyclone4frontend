fe8ebe8a44313bd66557aa4b7bf074ee4433e28a
ArjunAk1234 <144267463+ArjunAk1234@users.noreply.github.com>
2025-02-06 21:14:06 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,9 @@
-// // import { NgModule } from '@angular/core';
-// // import { RouterModule, Routes } from '@angular/router';
-// // import { LoginComponent } from './login/login.component';
 
-// // const routes: Routes = [
-// //   {
-// //     path: '',
-// //     component: LoginComponent,
-// //   },
-// // ];
-
-// // @NgModule({
-// //   imports: [RouterModule.forRoot(routes)],
-// //   exports: [RouterModule],
-// // })
-// // export class AppRoutingModule {}
-
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { LoginComponent } from './login/login.component';
-// import { HomeComponent } from './home/home.component';
-// import { AuthGuard } from './auth.guard';
-
-// const routes: Routes = [
-//     { path: 'login', component: LoginComponent },
-//     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-//     { path: '', redirectTo: '/login', pathMatch: 'full' }
-// ];
-
-// @NgModule({
-//     imports: [RouterModule.forRoot(routes)],
-//     exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './auth.guard';  // Import the AuthGuard
+// import { AuthGuard } from './auth.guard';  // Import the AuthGuard
 import { SignupComponent } from './signup/signup.component';
 
 
